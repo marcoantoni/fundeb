6586b7c3115b80e4e9672f6d58a9e4de97c7cf2b
Marco Antoni <marco.antoni910@gmail.com>
2019-09-05 11:29:26 -0300

    Removendo o mapeando de rotas com resource. Mapeando apenas as rotas necessárias

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('completo');
-});
 
+// rota para obter os todos os municipios de um estado
+Route::get('municipios/{id}', 'MunicipioController@show');
 
-Route::get('/municipios/{id}', 'MunicipioController@getMunicipios');
 Route::resource('despesa', 'DespesaController');
-Route::resource('matricula', 'MatriculaController');
+
+
 
 // só usada para obter os estados
-Route::resource('estados', 'EstadoController');
+//Route::get('estados/{id}', 'EstadoController@show');
 
-Route::get('/estimativas/', 'EstimativaController@index'); 
-Route::get('/estimativas/{id_estado}/ano/{ano}', 'EstimativaController@show'); 
+Route::get('estimativas/', 'EstimativaController@index'); 
+Route::get('estimativas/{id_estado}/ano/{ano}', 'EstimativaController@show'); 
 
 
-Route::get('/despesa/relatorio/{id}', 'DespesaController@getRelatorio');
+Route::get('despesa/relatorio/{id}', 'DespesaController@getRelatorio');
 
-Route::get('/matricula/indicadores/{id}', 'MatriculaController@getIndicadores');
-Route::get('/matricula/indicadores/{id}/estado', 'MatriculaController@getIndicadoresEstado');
+Route::get('matricula', 'MatriculaController@index');
+Route::get('matricula/{id}', 'MatriculaController@getMatriculas');
 Route::get('matricula/{id_estado}/estado', 'MatriculaController@getMatriculasEstados');
+Route::get('matricula/indicadores/{id}', 'MatriculaController@getIndicadores');
+Route::get('matricula/indicadores/{id}/estado', 'MatriculaController@getIndicadoresEstado');
